d94c3a0e4c7c99f78cda285f6fd40d65184bdc56
mhmmdiqbaaaal <iqbalsektor5@gmail.com>
2022-06-23 20:47:57 +0700

    Project UAS WP III

---
+++ kasir-bestcomp/application/views/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+<strong>Copyright &copy; 2022 <a href="#">BEST COMPUTER</a>.</strong>
+All rights reserved.
+<div class="float-right d-none d-sm-inline-block">
+  <b>AdminLTE Version</b> 3.0.2
+</div>
+</footer>
